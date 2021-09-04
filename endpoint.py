75ae6e121db632d19f881312353afba0e5f7f694
Kwang Ho Hur <hurxx018@gmail.com>
2021-09-04 03:30:28 +0000

    update endpoint.py

--- endpoint.py
+++ endpoint.py
@@ -3,56 +3,56 @@ import json
 
 # URL for the web service, should be similar to:
 # 'http://8530a665-66f3-49c8-a953-b82a2d312917.eastus.azurecontainer.io/score'
-scoring_uri = ''
+scoring_uri = 'http://c8e4ed37-7586-4c5b-be97-849769762522.southcentralus.azurecontainer.io/score'
 # If the service is authenticated, set the key or token
-key = ''
+key = 'FHCWxNTnEsJu76BM6i2KTW2Gqccb67cJ'
 
 # Two sets of data to score, so we get two results back
 data = {"data":
         [
           {
-            "age": 17,
-            "campaign": 1,
-            "cons.conf.idx": -46.2,
-            "cons.price.idx": 92.893,
-            "contact": "cellular",
-            "day_of_week": "mon",
-            "default": "no",
-            "duration": 971,
-            "education": "university.degree",
-            "emp.var.rate": -1.8,
-            "euribor3m": 1.299,
-            "housing": "yes",
+            "age": "17",
             "job": "blue-collar",
-            "loan": "yes",
             "marital": "married",
+            "education": "university.degree",
+            "default": "no",
+            "housing": "yes",
+            "loan": "yes",
+            "contact": "cellular",
             "month": "may",
-            "nr.employed": 5099.1,
-            "pdays": 999,
+            "day_of_week": "mon",
+            "duration": "971",
+            "campaign": "1",
+            "pdays": "999",
+            "previous": "1",
             "poutcome": "failure",
-            "previous": 1
+            "emp.var.rate": "-1.8",
+            "cons.price.idx": "92.893",
+            "cons.conf.idx": "-46.2",
+            "euribor3m": "1.299",
+            "nr.employed": "5099.1"
           },
           {
-            "age": 87,
-            "campaign": 1,
-            "cons.conf.idx": -46.2,
-            "cons.price.idx": 92.893,
-            "contact": "cellular",
-            "day_of_week": "mon",
-            "default": "no",
-            "duration": 471,
-            "education": "university.degree",
-            "emp.var.rate": -1.8,
-            "euribor3m": 1.299,
-            "housing": "yes",
+            "age": "87",
             "job": "blue-collar",
-            "loan": "yes",
             "marital": "married",
+            "education": "university.degree",
+            "default": "no",
+            "housing": "yes",
+            "loan": "yes",
+            "contact": "cellular",
             "month": "may",
-            "nr.employed": 5099.1,
-            "pdays": 999,
+            "day_of_week": "mon",
+            "duration": "471",
+            "campaign": "1",
+            "pdays": "999",
+            "previous": "1",
             "poutcome": "failure",
-            "previous": 1
+            "emp.var.rate": "-1.8",
+            "cons.price.idx": "92.893",
+            "cons.conf.idx": "-46.2",
+            "euribor3m": "1.299",
+            "nr.employed": "5099.1"
           },
       ]
     }
